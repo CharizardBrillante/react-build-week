3963f75bf25b887818533458330836b5176f56f5
Flavio Pinto <flaviopnt@hotmail.it>
2022-09-19 17:54:32 +0200

    [FEAT] Begin profile card

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
 export const GET_USERS = "GET_USERS";
 export const GET_EXPERIENCES = "GET_EXPERIENCES";
+export const LOGIN = "LOGIN";
 
 export const getUsersAction = (users) => ({
     type: GET_USERS,
@@ -9,4 +10,9 @@ export const getUsersAction = (users) => ({
 export const getExperiencesAction = (exp) => ({
   type: GET_EXPERIENCES,
   payload: exp
+})
+
+export const loginAction = (usr) => ({
+  type: LOGIN,
+  payload: usr
 })
