ef02e44ba7f6106a34ea6b7ea093e3a4dbadbe50
CharizardBrillante <106075512+CharizardBrillante@users.noreply.github.com>
2022-09-19 11:09:52 +0200

    react-bw-start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
