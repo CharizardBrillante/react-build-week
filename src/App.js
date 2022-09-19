db58ddb7b57b33f905a09b21bd62f09e23cdc31a
ChiaraSoddu <chiara.soddu@hotmail.com>
2022-09-19 17:55:39 +0200

    Navbar build-week react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MyNavbar from './components/MyNavbar';
+
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-
+      <MyNavbar/>
     </div>
+    </BrowserRouter>
   );
 }
 
