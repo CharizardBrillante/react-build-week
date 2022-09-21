be3470da53e890c295cc4013a82266430fa4f171
Riccardo Bonaiuto <riccardo.bonaiuto@gmail.com>
2022-09-21 20:31:03 +0200

    footer copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,60 +3,73 @@ const Footer = () => {
     <div className="footer">
       <div className="footer_container">
         <div className="footer_row">
-          
-            <div className="footer_links">
-              <ul>
-                <li>
-                  <a href="">About</a>
-                </li>
-                <li>
-                  <a href="">Community Guidelines</a>
-                </li>
-                <li>
-                  <a href="">Privacy & Terms </a>
-                </li>
-                <li>
-                  <a href="">Sales Solutions</a>
-                </li>
-                <li>
-                  <a href="">Safety Center</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer_links">
-              <ul>
-                <li>
-                  <a href="">Accessibility</a>
-                </li>
-                <li>
-                  <a href="">Careers</a>
-                </li>
-                <li>
-                  <a href="">Ad Choices</a>
-                </li>
-                <li>
-                  <a href="">Mobile</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer_links">
-              <ul>
-                <li>
-                  <a href="">Talent Solutions</a>
-                </li>
-                <li>
-                  <a href="">Marketing Solutions</a>
-                </li>
-                <li>
-                  <a href="">Advertising</a>
-                </li>
-                <li>
-                  <a href="">Small Business</a>
-                </li>
-              </ul>
-            </div>
-          
+          <div className="footer_links">
+            <ul>
+              <li>
+                <a href="">About</a>
+              </li>
+              <li>
+                <a href="">Community Guidelines</a>
+              </li>
+              <li>
+                <a href="">Privacy & Terms </a>
+              </li>
+              <li>
+                <a href="">Sales Solutions</a>
+              </li>
+              <li>
+                <a href="">Safety Center</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer_links">
+            <ul>
+              <li>
+                <a href="">Accessibility</a>
+              </li>
+              <li>
+                <a href="">Careers</a>
+              </li>
+              <li>
+                <a href="">Ad Choices</a>
+              </li>
+              <li>
+                <a href="">Mobile</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer_links">
+            <ul>
+              <li>
+                <a href="">Talent Solutions</a>
+              </li>
+              <li>
+                <a href="">Marketing Solutions</a>
+              </li>
+              <li>
+                <a href="">Advertising</a>
+              </li>
+              <li>
+                <a href="">Small Business</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer_links">
+            <ul>
+              <li>
+                <a href="" id="questions">Questions?</a> 
+                <p>Visit our Help Center.</p>
+              </li>
+              <li>
+                <a href="" id="questions">Manage your account and privacy</a> 
+                <p>Go to your Settings.</p>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <div className="copyright">
+        <p>LinkedIn Corporation © 2022</p>
       </div>
     </div>
   );
